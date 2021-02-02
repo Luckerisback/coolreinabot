b27c58353791805428549fc5b6f83c01a4b4c72e
Luckerisback <61817870+Luckerisback@users.noreply.github.com>
2021-02-03 00:41:15 +0700

    Update main.py

--- main.py
+++ main.py
@@ -14,13 +14,4 @@ async def елимясомужики(ctx):
     await ctx.send(f'Пивом запивали!, {author.mention}!')
 
 
-@bot.command()
-async def fox(ctx):
-    response = requests.get('https://some-random-api.ml/img/fox') # Get-запрос
-    json_data = json.loads(response.text) # Извлекаем JSON
-
-    embed = discord.Embed(color = 0xff9900, title = 'Random Fox') # Создание Embed'a
-    embed.set_image(url = json_data['link']) # Устанавливаем картинку Embed'a
-    await ctx.send(embed = embed) # Отправляем Embed
-
 bot.run(setting['token'])
