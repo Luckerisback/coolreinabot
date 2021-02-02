afe3e447290211107f753788eab532b003cb113f
Luckerisback <61817870+Luckerisback@users.noreply.github.com>
2021-02-02 12:11:04 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 setting = {
-    'token': os.environ.get('ODA1ODI0MDQ4Mjg1NjE0MDgw.YBggIA.ttTEZn3QkmaMHCppTs9-MJQBE28'),
+    'token': os.environ.get('BOT_TOKEN'),
     'bot': 'myfirstmegabot123',
     'id': '805824048285614080',
     'prefix': '!'
